f750b97321c0dfcc99b88b14d8bc5c13e09c51cf
Salah Mohamed <salah.mohamed0404@gmail.com>
2024-06-03 09:41:13 +0300

    chore: Remove unnecessary transition duration in MouseAxises component

--- src/components/Canvas/MouseAxises.jsx
+++ src/components/Canvas/MouseAxises.jsx
@@ -2,11 +2,11 @@ export default function MouseAxises({ mousePosition }) {
   return (
     <>
       <div
-        className="absolute inset-y-0 w-1 border-l-2 border-dashed border-neutral-500 transition-all duration-75 ease-linear"
+        className="absolute inset-y-0 w-1 border-l-2 border-dashed border-neutral-500"
         style={{ left: `${mousePosition.x}px` }}
       ></div>
       <div
-        className="absolute inset-x-0 h-1 border-t-2 border-dashed border-neutral-500 transition-all duration-75 ease-linear"
+        className="absolute inset-x-0 h-1 border-t-2 border-dashed border-neutral-500"
         style={{ top: `${mousePosition.y}px` }}
       ></div>
     </>
