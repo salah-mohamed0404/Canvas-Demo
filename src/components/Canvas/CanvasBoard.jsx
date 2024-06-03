569bf2b8cdb962a5739e1ae63ce6f7f768301e5f
Salah Mohamed <salah.mohamed0404@gmail.com>
2024-06-03 17:05:35 +0300

    feat: Add SolarPanelAreas and CurrentAddingSolarPanelArea components to CanvasBoard

--- src/components/Canvas/CanvasBoard.jsx
+++ src/components/Canvas/CanvasBoard.jsx
@@ -5,6 +5,8 @@ import StrokedRect from "./StrokedRect";
 import TextWithBackground from "./TextWithBackground";
 import { throttle } from "../../utils/throttle";
 import MouseAxises from "./MouseAxises";
+import SolarPanelAreas from "./SolarPanelAreas";
+import CurrentAddingSolarPanelArea from "./SolarPanelAreas/CurrentAddingSolarPanelArea";
 
 const stageWidth = window.innerWidth;
 const stageHeight = window.innerHeight;
@@ -27,10 +29,43 @@ export default function CanvasBoard() {
   const [isHovering, setIsHovering] = useState(false);
   const [isAddingSolarPanelArea, setIsAddingSolarPanelArea] = useState(false);
   const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
+  const [rectangles, setRectangles] = useState([]);
+  const [startPos, setStartPos] = useState(null);
+  const [currentPos, setCurrentPos] = useState(null);
+
+  const handleMouseEnter = () => setIsHovering(true);
+  const handleMouseLeave = () => setIsHovering(false);
 
   const handleMouseMove = throttle((event) => {
+    if (!isAddingSolarPanelArea) return;
+
     setMousePosition({ x: event.clientX, y: event.clientY });
+    setCurrentPos({ x: event.clientX, y: event.clientY });
   }, 5);
+
+  const handleMouseDown = (event) => {
+    if (!isAddingSolarPanelArea) return;
+
+    setStartPos({ x: event.clientX, y: event.clientY });
+  };
+
+  const handleMouseUp = () => {
+    if (!isAddingSolarPanelArea && !startPos) return;
+
+    setIsAddingSolarPanelArea(false);
+
+    setRectangles([
+      ...rectangles,
+      {
+        x: Math.min(startPos.x, currentPos.x),
+        y: Math.min(startPos.y, currentPos.y),
+        width: Math.abs(startPos.x - currentPos.x),
+        height: Math.abs(startPos.y - currentPos.y),
+      },
+    ]);
+    setStartPos(null);
+    setCurrentPos(null);
+  };
 
   const handleKeyDown = (event) => {
     // Escape key Code is 27
@@ -39,15 +74,14 @@ export default function CanvasBoard() {
     }
   };
 
-  const handleMouseEnter = () => setIsHovering(true);
-  const handleMouseLeave = () => setIsHovering(false);
-
   return (
     <div
-      className={`grid h-dvh w-dvw place-content-center bg-neutral-300 ${
+      className={`grid h-dvh w-dvw place-content-center overflow-hidden bg-neutral-300 ${
         isHovering ? "cursor-move" : ""
       } ${isAddingSolarPanelArea ? "cursor-crosshair" : ""}`}
       onMouseMoveCapture={handleMouseMove}
+      onMouseDown={handleMouseDown}
+      onMouseUp={handleMouseUp}
       onKeyDown={handleKeyDown}
       tabIndex={0}
     >
@@ -75,10 +109,25 @@ export default function CanvasBoard() {
               />
             }
           />
+
+          <SolarPanelAreas
+            rectangles={rectangles}
+            roof={{
+              width: mainRectWidth,
+              height: mainRectHeight,
+              ...mainRectCoords,
+            }}
+          />
+
+          <CurrentAddingSolarPanelArea
+            startPos={startPos}
+            currentPos={currentPos}
+          />
         </Layer>
       </Stage>
 
       <button
+        type="button"
         className="bg-primary-500 absolute right-5 top-5 rounded-md bg-slate-800 p-2 px-6 py-3 text-white"
         onClick={() => setIsAddingSolarPanelArea(true)}
       >
