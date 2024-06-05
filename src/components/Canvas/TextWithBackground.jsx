9545a94beb9eaa6e363ed702ab31920d21c73998
Salah Mohamed <salah.mohamed0404@gmail.com>
2024-06-05 21:24:18 +0300

    refactor: Update TextWithBackground component to dynamically adjust position and size based on scale

--- src/components/Canvas/TextWithBackground.jsx
+++ src/components/Canvas/TextWithBackground.jsx
@@ -1,20 +1,27 @@
 import { Group, Rect, Text } from "react-konva";
 
-export default function TextWithBackground({ stageWidth, stageHeight }) {
+export default function TextWithBackground({
+  strokedRectWidth,
+  strokedRectHeight,
+  strokedRectCoords,
+  scale,
+}) {
   return (
     <Group>
       <Rect
-        x={stageWidth / 2 - 40} // Half of the stage width minus half of the Rect width to center it horizontally
-        y={stageHeight / 2 - 150 + 285} // Adjusted y coordinate to center the text in the bottom line of the stroked rect
+        x={strokedRectCoords.x + strokedRectWidth / 2 - 40 * scale}
+        y={strokedRectCoords.y + strokedRectHeight - 15 * scale}
         width={80}
         height={30}
         fill="#B8860B"
+        scale={{ x: scale, y: scale }}
       />
       <Text
-        x={stageWidth / 2 - 22} // Half of the stage width minus half of the Rect width to center it horizontally
-        y={stageHeight / 2 - 150 + 295} // Adjusted y coordinate to center the text in the bottom line of the stroked rect
+        x={strokedRectCoords.x + strokedRectWidth / 2 - 22 * scale}
+        y={strokedRectCoords.y + strokedRectHeight - 5 * scale}
         text="Randzon"
         fill="white"
+        fontSize={12 * scale}
       />
     </Group>
   );
