8bd54c3ce519b9e337215b02d9c0b17e8aa6be51
Salah Mohamed <salah.mohamed0404@gmail.com>
2024-06-05 15:31:08 +0300

    refactor: Update SolarPanelAreas component to handle selection and onChange events

--- src/components/Canvas/SolarPanelAreas/index.jsx
+++ src/components/Canvas/SolarPanelAreas/index.jsx
@@ -1,11 +1,20 @@
 import SolarPanelArea from "./SolarPanelArea";
 
-export default function SolarPanelAreas({ rectangles, roof }) {
+export default function SolarPanelAreas({
+  rectangles,
+  roof,
+  selectRect,
+  selectedRect,
+  onChange,
+}) {
   return rectangles.map((rect, i) => (
     <SolarPanelArea
       key={`panels-area-${i}-${rect.x}-${rect.y}`}
       rect={rect}
       roof={roof}
+      selectRect={() => selectRect(i)}
+      isSelected={i === selectedRect}
+      onChange={onChange(i)}
     />
   ));
 }
