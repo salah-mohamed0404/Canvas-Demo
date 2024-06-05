0755b25251e38d79a882a0d71086a3ede2e6ee0b
Salah Mohamed <salah.mohamed0404@gmail.com>
2024-06-05 15:30:59 +0300

    refactor: Remove unnecessary Group component from StrokedRect in Canvas

--- src/components/Canvas/StrokedRect.jsx
+++ src/components/Canvas/StrokedRect.jsx
@@ -1,12 +1,12 @@
 import { memo } from "react";
-import { Group, Rect } from "react-konva";
+import { Rect } from "react-konva";
 
 export default memo(function StrokedRect({ x, y, width, height, title }) {
   return (
-    <Group>
+    <>
       <Rect x={x} y={y} width={width} height={height} stroke="#B8860B" />
 
       {title}
-    </Group>
+    </>
   );
 });
