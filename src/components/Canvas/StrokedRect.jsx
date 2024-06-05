b4947088251a473086501a234920c6abb1d4cc10
Salah Mohamed <salah.mohamed0404@gmail.com>
2024-06-05 21:24:06 +0300

    refactor: Update StrokedRect component to include strokeWidth prop

--- src/components/Canvas/StrokedRect.jsx
+++ src/components/Canvas/StrokedRect.jsx
@@ -1,10 +1,24 @@
 import { memo } from "react";
 import { Rect } from "react-konva";
 
-export default memo(function StrokedRect({ x, y, width, height, title }) {
+export default memo(function StrokedRect({
+  x,
+  y,
+  width,
+  height,
+  title,
+  scale,
+}) {
   return (
     <>
-      <Rect x={x} y={y} width={width} height={height} stroke="#B8860B" />
+      <Rect
+        x={x}
+        y={y}
+        width={width}
+        height={height}
+        stroke="#B8860B"
+        strokeWidth={2 * scale}
+      />
 
       {title}
     </>
