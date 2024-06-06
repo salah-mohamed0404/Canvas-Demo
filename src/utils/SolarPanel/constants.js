b716919ef81f62ea2b29b83aec11439527745fbb
Salah Mohamed <salah.mohamed0404@gmail.com>
2024-06-06 10:55:42 +0300

    refactor: Reorganize the solar panels utilts

---
+++ src/utils/SolarPanel/constants.js
@@ -0,0 +1,12 @@
+export const SOLAR_PANEL_STATUS = {
+  NORMAL: "normal",
+  WARNING: "warning",
+  POSSIBLE: "possible",
+};
+
+export const SOLAR_PANEL_STATUS_COLOR = {
+  normal: "#444",
+  warning: "#7a3333",
+  possible: "#444",
+  removed: "#fff",
+};
---
+++ src/utils/SolarPanel/index.js
@@ -0,0 +1,3 @@
+export * from "./constants";
+export * from "./checks";
+export * from "./draws";
