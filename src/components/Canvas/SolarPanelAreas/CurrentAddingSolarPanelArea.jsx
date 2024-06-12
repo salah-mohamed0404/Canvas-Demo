77f3071d8d621573ab28ba4db0e085da1f38c4ee
Salah Mohamed <salah.mohamed0404@gmail.com>
2024-06-12 16:12:30 +0300

    refactor: add isCurrentAdding flag to current adding solar panel area component

--- src/components/Canvas/SolarPanelAreas/CurrentAddingSolarPanelArea.jsx
+++ src/components/Canvas/SolarPanelAreas/CurrentAddingSolarPanelArea.jsx
@@ -16,6 +16,7 @@ export default function CurrentAddingSolarPanelArea({
           height: Math.abs(startPos.y - currentPos.y),
         }}
         roof={roof}
+        isCurrentAdding
       />
     )
   );
