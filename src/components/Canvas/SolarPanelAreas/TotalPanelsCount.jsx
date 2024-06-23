6f621e427dbe8a872412da38f83ee021b4c612f2
Salah Mohamed <salah.mohamed0404@gmail.com>
2024-06-23 11:47:41 +0300

    feature: build TotalPanelsCount components

--- src/components/Canvas/SolarPanelAreas/TotalPanelsCount.jsx
+++ src/components/Canvas/SolarPanelAreas/TotalPanelsCount.jsx
@@ -1,9 +1,25 @@
+import { memo } from "react";
 import { Html } from "react-konva-utils";
 
-export default function TotalPanelsCount({ totalCountX, totalCountY }) {
+export default memo(function TotalPanelsCount({
+  position,
+  totalCountX,
+  totalCountY,
+}) {
   return (
     <Html>
-      {totalCountX} | {totalCountY}
+      <div
+        className="absolute flex -translate-y-1/2 flex-col justify-center bg-gray-200 p-2 text-sm *:whitespace-nowrap"
+        style={{
+          top: `${position.y}px`,
+          left: `${position.x}px`,
+        }}
+      >
+        {/* {totalCountX} | {totalCountY} */}
+        <p>Panels Y: {totalCountY}</p>
+        <p>Panels X: {totalCountX}</p>
+        <p>Total Panels: {totalCountX * totalCountY}</p>
+      </div>
     </Html>
   );
-}
+});
