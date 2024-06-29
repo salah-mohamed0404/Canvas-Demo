cf7fe473b865c5eb30efedf168e87e55f8814378
Salah Mohamed <salah.mohamed0404@gmail.com>
2024-06-29 16:08:24 +0300

    feat: init steps

--- src/pages/ParallelSystem.jsx
+++ src/pages/ParallelSystem.jsx
@@ -1,7 +1,29 @@
+import PositionStep from "@/components/ParallelSystem/PositionStep";
+import ConfigurationStep from "@/components/ParallelSystem/PositionStep/ConfigurationStep";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+
 export default function ParallelSystem() {
+  const [majorStep, setMajorStep] = useState(0);
+  const { register, getValues } = useForm();
+
+  const handleNextMajorStep = () => {
+    setMajorStep((prev) => prev + 1);
+  };
+
   return (
-    <div>
-      <h1>ParallelSystem</h1>
-    </div>
+    <main>
+      <form>
+        {majorStep === 0 && (
+          <PositionStep
+            register={register}
+            getValues={getValues}
+            onNextMajorStep={handleNextMajorStep}
+          />
+        )}
+
+        {majorStep === 1 && <ConfigurationStep />}
+      </form>
+    </main>
   );
 }
