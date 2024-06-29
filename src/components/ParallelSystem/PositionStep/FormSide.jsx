384f20c3ef47dd592231460bfee92c6f95955ca0
Salah Mohamed <salah.mohamed0404@gmail.com>
2024-06-29 16:09:42 +0300

    feat: init FormSide ui component

---
+++ src/components/ParallelSystem/PositionStep/FormSide.jsx
@@ -0,0 +1,15 @@
+export default function FormSide() {
+  return (
+    <>
+      <section>
+        <header>
+          <h2>Position</h2>
+          <p>
+            Enter position in the field below or on the map to retrieve climate
+            loads.
+          </p>
+        </header>
+      </section>
+    </>
+  );
+}
