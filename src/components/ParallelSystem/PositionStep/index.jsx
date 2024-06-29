3c14c86f4e5864a32410ef2d69fcd322d3795a48
Salah Mohamed <salah.mohamed0404@gmail.com>
2024-06-29 16:10:06 +0300

    feat: init PositionStep ui component

---
+++ src/components/ParallelSystem/PositionStep/index.jsx
@@ -0,0 +1,11 @@
+import FormSide from "./FormSide";
+
+export default function PositionStep() {
+  return (
+    <div className="flex h-dvh">
+      <div className="w-1/2">
+        <FormSide />
+      </div>
+    </div>
+  );
+}
