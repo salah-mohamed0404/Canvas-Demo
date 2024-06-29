0e9a6a04acc16365e454ab66b6f11cecc06479e5
Salah Mohamed <salah.mohamed0404@gmail.com>
2024-06-29 15:01:32 +0300

    feat: add RouterProvider to the App

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
-import CanvasBoard from "./components/Canvas/CanvasBoard";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./router";
 
 function App() {
-  return <CanvasBoard />;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
