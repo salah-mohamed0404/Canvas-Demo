1f7eb1eb755c21f7eca6d1cc029125c60d8f9b5a
Salah Mohamed <salah.mohamed0404@gmail.com>
2024-07-01 17:00:50 +0300

    feat: add shadcn/ui Input component

--- src/components/ui/input.jsx
+++ src/components/ui/input.jsx
@@ -6,7 +6,7 @@ const Input = forwardRef(({ className, type, ...props }, ref) => {
     <input
       type={type}
       className={cn(
-        "border-input placeholder:text-muted-foreground focus-visible:ring-ring flex h-9 w-full rounded-md border bg-transparent px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium focus-visible:outline-none focus-visible:ring-1 disabled:cursor-not-allowed disabled:opacity-50",
+        "flex h-9 w-full rounded-none border border-input bg-transparent px-4 py-5 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50",
         className,
       )}
       ref={ref}
